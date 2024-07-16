d2ed7c31fd6b5f4edc43c8df103f14727e608656
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2024-07-16 13:57:04 +0700

    update

--- config/snappy.php
+++ config/snappy.php
@@ -35,7 +35,7 @@ return [
 
     'pdf' => [
         'enabled' => true,
-        'binary'  => '/usr/local/bin/wkhtmltopdf',
+        'binary'  => '/usr/local/bin/wkhtmltopdf-amd64',
         'timeout' => false,
         'options' => [],
         'env'     => [],
